fc727ed83eca7f7f005d5dcd4117c86cd53dc1eb
Pablo Trujillo <005468537@coyote.csusb.edu>
2021-02-22 19:28:47 -0800

    first commit

---
+++ Index/Index/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Index
+//
+//  Created by Pablo Trujillo on 2/22/21.
+//  Copyright © 2021 Pablo Trujillo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
